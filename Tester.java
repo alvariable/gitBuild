5a3c8ecfbf1f45c9f1c45a85b6b77455f5329cc8
BenR <90284651+AsianUser@users.noreply.github.com>
2023-09-12 10:58:52 -0700

    Ran Tester & Added json
    json
    
    tester - runs
     - bug: index.txt does not have 2nd file added in

--- Tester.java
+++ Tester.java
@@ -6,10 +6,9 @@ public class Tester {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Index init = new Index();
         init.initializeProject();
-        init.addBlob("/Users/ardendoyle/Documents/personal/coding/Honors Topics/gitBuild/testFile.txt");
-        // init.addBlob("/Users/ardendoyle/Documents/personal/coding/Honors
-        // Topics/gitBuild/file.txt");
-        ;
-        init.removeBlob("/Users/ardendoyle/Documents/personal/coding/Honors Topics/gitBuild/file.txt");
+        init.addBlob(".\\testFile.txt");
+        init.addBlob(".\\file.txt");
+
+        init.removeBlob(".\\file.txt");
     }
 }
